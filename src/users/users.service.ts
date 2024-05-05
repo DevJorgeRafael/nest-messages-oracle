fe3222733d098ac3300aaf6ac28006a92b8cfb48
Jorge Rafael Rosero <rafaelrosero.r.r2408@gmail.com>
2024-05-05 13:16:26 -0500

    http exceptions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -10,7 +10,15 @@ export class UsersService {
 
     constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
 
-    createUser(user: CreateUserDto){
+    async createUser(user: CreateUserDto){
+
+        const userFound = await this.userRepository.findOne({
+            where: {
+                email: user.email
+            }
+        })
+        if(userFound) return new HttpException('User already exists', HttpStatus.CONFLICT)
+
         const newUser = this.userRepository.create(user);
         return this.userRepository.save(newUser);
     }
@@ -19,20 +27,35 @@ export class UsersService {
         return this.userRepository.find();
     }
 
-    getUser(id: number) {
-        return this.userRepository.findOne({
+    async getUser(id: number) {
+        const userFound = await this.userRepository.findOne({
             where: {
                 id
             }
         })
+        if(!userFound) return new HttpException('User not found', HttpStatus.NOT_FOUND)
+
+        return userFound;            
     }
 
-    deleteUser(id: number) {
-        return this.userRepository.delete({ id });
+    async deleteUser(id: number) {
+        const result = await this.userRepository.delete({ id })
+
+        if(result.affected === 0) return new HttpException('User not found', HttpStatus.NOT_FOUND)
+        return result    
     }
 
-    updateUser(id: number, user: UpdateUserDto ) {
-        return this.userRepository.update({ id }, user);
+    async updateUser(id: number, user: UpdateUserDto ) {
+        const userFound = await this.userRepository.findOne({
+            where: {
+                id
+            }
+        })
+
+        if(!userFound) return new HttpException('User not found', HttpStatus.NOT_FOUND)
+
+        const updateUser = Object.assign(userFound, user)
+        return this.userRepository.save(updateUser)
     }
 
 }
