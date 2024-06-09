aff6cf914f8325c1d9676bd8acce9c52f459a824
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2024-06-09 17:31:46 -0500

    learn the Swagger documentation for nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Config for Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Documentación API - Social-UTN')
+    .setDescription('Documentación de la API de Social-UTN')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   await app.listen(3000);
 }
 bootstrap();
--- src/role/controllers/roles.controller.ts
+++ src/role/controllers/roles.controller.ts
@@ -2,32 +2,48 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from 
 import { RoleService } from '../services/roles.service';
 import { CreateRoleDto } from '../dto/create-role.dto';
 import { UpdateRoleDto } from '../dto/update-role.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('roles')
 @Controller('roles')
 export class RoleController {
   constructor(private readonly roleService: RoleService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new role' })
+  @ApiResponse({ status: 201, description: 'The role has been successfully created.'})
+  @ApiResponse({ status: 403, description: 'Forbidden.'})
   createRole(@Body() createRoleDto: CreateRoleDto) {
     return this.roleService.create(createRoleDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all roles' })
+  @ApiResponse({ status: 200, description: 'Return all roles.'})
   getRoles() {
     return this.roleService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a role by id' })
+  @ApiResponse({ status: 200, description: 'Return the role by id.'})
+  @ApiResponse({ status: 404, description: 'Role not found.'})
   getRole(@Param('id', ParseIntPipe) id: number) {
     return this.roleService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a role by id' })
+  @ApiResponse({ status: 200, description: 'The role has been successfully updated.'})
+  @ApiResponse({ status: 404, description: 'Role not found.'})
   updateRole(@Param('id', ParseIntPipe) id: number, @Body() updateRoleDto: UpdateRoleDto) {
     return this.roleService.update(id, updateRoleDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a role by id' })
+  @ApiResponse({ status: 200, description: 'The role has been successfully deleted.'})
+  @ApiResponse({ status: 404, description: 'Role not found.'})
   deleteRole(@Param('id', ParseIntPipe) id: number) {
     return this.roleService.remove(id);
   }
